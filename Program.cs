a0a23bbae3d3c799b65f42e39f8d729e032109c2
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-07-09 09:51:53 -0500

    Added dictionary with a word.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace dictionary_list
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      var dictionaryOfWords = new List<Dictionary<string, string>>();
+
+      //  Each word needs:
+      //    word
+      //    definition
+      //    part of speech
+      //    example sentence
+
+      var excitedWord = new Dictionary<string, string>();
+
+      excitedWord.Add("word", "excited");
+      excitedWord.Add("definition", "having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness");
+      excitedWord.Add("part of speech", "adjective");
+      excitedWord.Add("example sentence", "I am excited to learn C#!");
+
+
+      dictionaryOfWords.Add(excitedWord);
+
     }
+  }
 }
